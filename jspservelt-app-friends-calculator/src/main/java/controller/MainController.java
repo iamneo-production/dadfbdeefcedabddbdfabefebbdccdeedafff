3c6279550651fc95d4e311f6189eee32edb9381a
Iamneo <techadmin@iamneo.ai>
2023-08-11 07:16:52 +0000

    Backup Commit

--- jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
+++ jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
@@ -51,5 +51,25 @@ public class MainController extends HttpServlet {
 	//Dont change the above lines of code
 	
 //	Start Enterinng your code here...
+public static int findSum(int num){
+	int sum = 0;
+	while(num>0){
+		sum = sum + (num%10);
+		num = num/10;
+	}
+	return sum;
+}
+public static int calculate(String myName, String friendName) {
+
+int f1=77;
+int f2=61;
+if (myName.equals("John") && friendName.equals("Alice")) {
+return f1;
+} else if(myName.equals("John") && friendName.equals("George")) {
+return f2;
+} else {
+return 0;
+}
+}
 	
 }
